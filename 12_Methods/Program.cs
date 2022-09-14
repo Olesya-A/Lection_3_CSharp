d4a0b3b0292474397674ea6310ee25b0163b1e56
Olesya <lesusha1@yandex.ru>
2022-09-14 17:56:34 +0300

    add 12_Methods цикл for и работа с текстом

--- 12_Methods/Program.cs
+++ 12_Methods/Program.cs
@@ -17,7 +17,7 @@ void Method2(string msg)
 void Method21(string msg, int count)
 {
     int i = 0;
-    while(i < count)
+    while (i < count)
     {
         Console.WriteLine(msg);
         i++;
@@ -38,16 +38,76 @@ int Method3()
 
 // Вид 4 - это методы, которые что-то принимают и что-то возвращают.
 
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = String.Empty;
+//     while(i < count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
+// string res = Method4(10, "asdf");
+// Console.WriteLine(res);
+
+// ЦИКЛ FOR
+
 string Method4(int count, string text)
 {
-    int i = 0;
     string result = String.Empty;
-    while(i < count)
+    for (int i = 0; i < count; i++)
     {
         result = result + text;
-        i++;
     }
     return result;
 }
 // string res = Method4(10, "asdf");
 // Console.WriteLine(res);
+
+// ЦИКЛ В ЦИКЛЕ
+
+// for(int i = 2; i <= 10;  i++)
+//     {
+//         for(int j = 2; j <= 10;  j++)
+//         {
+//             Console.WriteLine($"{i} x {j} = {i*j}");
+//         }
+//         Console.WriteLine();
+//     }
+
+
+//РАБОТА С ТЕКСТОМ
+// Дан текст. В тексте нужно все пробелы заменить чёрточками,
+// маленькие буквы “к” заменить большими “К”,
+// а большие “С” маленькими “с”.
+
+string text = "— Я думаю, — сказал князь, улыбаясь, — что,"
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля."
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s = “qwerty”
+// 012345
+// s[3] // r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue)
+        {
+            result = result + $"{newValue}";
+        }
+        else 
+        {
+            result = result + $"{text[i]}";
+        }
+    }
+    return result;
+}
+string newText = Replace(text, ' ','|');
+Console.WriteLine(newText);
